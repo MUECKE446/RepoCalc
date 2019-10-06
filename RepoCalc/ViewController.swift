b488651191c8de44aa1bdcb8000c1dd81437440a
Christian Muth <christian-muth@t-online.de>
2019-10-06 15:24:24 +0200

    IBAction und IBOutlet hinzugefügt

--- RepoCalc/ViewController.swift
+++ RepoCalc/ViewController.swift
@@ -10,6 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calculatorDisplay: UILabel!
+    
+    @IBAction func button0(_ sender: Any) {
+    }
+    
+    @IBAction func button1(_ sender: Any) {
+    }
+    
+    @IBAction func button2(_ sender: Any) {
+    }
+    
+    @IBAction func button3(_ sender: Any) {
+    }
+    
+    @IBAction func button4(_ sender: Any) {
+    }
+    
+    @IBAction func button5(_ sender: Any) {
+    }
+    
+    @IBAction func button6(_ sender: Any) {
+    }
+    
+    @IBAction func button7(_ sender: Any) {
+    }
+    
+    @IBAction func button8(_ sender: Any) {
+    }
+    
+    @IBAction func button9(_ sender: Any) {
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
