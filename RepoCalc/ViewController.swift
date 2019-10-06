c5bac94bf233327ee3383bd66958daf4820386a2
Christian Muth <christian-muth@t-online.de>
2019-10-06 17:01:15 +0200

    gedrückte Tasten werden im Display angezeigt

--- RepoCalc/ViewController.swift
+++ RepoCalc/ViewController.swift
@@ -13,43 +13,65 @@ class ViewController: UIViewController {
     @IBOutlet weak var calculatorDisplay: UILabel!
     
     @IBAction func button0(_ sender: Any) {
+        let tappedValue = "0"
+        showTappedValueInDisplay(tappedValue: tappedValue)
     }
     
     @IBAction func button1(_ sender: Any) {
+        let tappedValue = "1"
+        showTappedValueInDisplay(tappedValue: tappedValue)
     }
     
     @IBAction func button2(_ sender: Any) {
+        let tappedValue = "2"
+        showTappedValueInDisplay(tappedValue: tappedValue)
     }
     
     @IBAction func button3(_ sender: Any) {
+        let tappedValue = "3"
+        showTappedValueInDisplay(tappedValue: tappedValue)
     }
     
     @IBAction func button4(_ sender: Any) {
+        let tappedValue = "4"
+        showTappedValueInDisplay(tappedValue: tappedValue)
     }
     
     @IBAction func button5(_ sender: Any) {
+        let tappedValue = "5"
+        showTappedValueInDisplay(tappedValue: tappedValue)
     }
     
     @IBAction func button6(_ sender: Any) {
+        let tappedValue = "6"
+        showTappedValueInDisplay(tappedValue: tappedValue)
     }
     
     @IBAction func button7(_ sender: Any) {
+        let tappedValue = "7"
+        showTappedValueInDisplay(tappedValue: tappedValue)
     }
     
     @IBAction func button8(_ sender: Any) {
+        let tappedValue = "8"
+        showTappedValueInDisplay(tappedValue: tappedValue)
     }
     
     @IBAction func button9(_ sender: Any) {
+        let tappedValue = "9"
+        showTappedValueInDisplay(tappedValue: tappedValue)
     }
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func showTappedValueInDisplay(tappedValue : String) {
+        calculatorDisplay.text! += String(tappedValue)
+    }
+    
+    
+    
 }
 
